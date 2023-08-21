16a17e699d21a6daad08d62eac25cd96e4f4934e
Felix Schwarz <unuhz@student.kit.edu>
2023-08-21 20:07:07 +0200

    body class

---
+++ body.py
@@ -0,0 +1,9 @@
+class Body:
+
+    def __init__(self, positionX, positionY, velocityX, velocityY, mass):
+        self.x = positionX
+        self.y = positionY
+        self.velX = velocityX
+        self.velY = velocityY
+        self.mass = mass
+
