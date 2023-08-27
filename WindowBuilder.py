a28c6d8fbf8734cc49357303e01724cbf56e0457
Felix Schwarz <unuhz@student.kit.edu>
2023-08-27 14:45:46 +0200

    WindowBuilder with pyqt5

--- WindowBuilder.py
+++ WindowBuilder.py
@@ -1,34 +1,52 @@
-from tkinter import *
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QAction
+
+
+class Window(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Potential Fractals")
+        self.resize(400, 200)
+        self.centralWidget = QLabel("Hello, World")
+        self.centralWidget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+        self.setCentralWidget(self.centralWidget)
+        self._createactions()
+        self._createmenubar()
+
+    def _createactions(self):
+        # Creating actions using the second constructor
+        self.newAction = QAction("&New", self)
+        self.openAction = QAction("&Open...", self)
+        self.saveAction = QAction("&Save", self)
+        self.exitAction = QAction("&Exit", self)
+        self.copyAction = QAction("&Copy", self)
+        self.pasteAction = QAction("&Paste", self)
+        self.cutAction = QAction("C&ut", self)
+        self.helpContentAction = QAction("&Help Content", self)
+        self.aboutAction = QAction("&About", self)
+
+    def _createmenubar(self):
+        menuBar = self.menuBar()
+        fileMenu = menuBar.addMenu("&File")
+        fileMenu.addAction(self.newAction)
+        fileMenu.addAction(self.openAction)
+        fileMenu.addAction(self.saveAction)
+        fileMenu.addAction(self.exitAction)
+
+        editMenu = menuBar.addMenu("&Edit")
+        editMenu.addAction(self.copyAction)
+        editMenu.addAction(self.pasteAction)
+        editMenu.addAction(self.cutAction)
+
+        helpMenu = menuBar.addMenu("&Help")
+        helpMenu.addAction(self.helpContentAction)
+        helpMenu.addAction(self.aboutAction)
 
 
 def createWindow():
-    root = Tk()
-    root.title("Potential Fractals")
-    root.geometry('1500x900')
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
 
-    menubar = create_menu_bar(root)
-
-    file_menu = create_menu(menubar, "File")
-    create_menu_item(file_menu, "New")
-    create_menu_item(file_menu, "Exit", root.quit)
-
-    file_menu = create_menu(menubar, "Edit")
-    file_menu = create_menu(menubar, "Help")
-
-    root.mainloop()
-
-
-def create_menu_item(file_menu: Menu, label: str, command=None):
-    file_menu.add_command(label=label, command=command)
-
-
-def create_menu(menubar: Menu, label: str):
-    file_menu = Menu(menubar, tearoff=0)
-    menubar.add_cascade(label=label, menu=file_menu)
-    return file_menu
-
-
-def create_menu_bar(root):
-    menubar = Menu(root)
-    root.config(menu=menubar)
-    return menubar
+    sys.exit(app.exec_())
